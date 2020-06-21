fb32743dcc1e90b383cc6b0f168cd7e110af5974
bronisMateusz <mateusz.bronis99@gmail.com>
2020-06-21 14:02:47 +0200

    Menu reorganising

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,42 +1,46 @@
 import React from 'react';
+import { Link, animateScroll as scroll } from 'react-scroll';
 import { Container, Nav, Row } from 'react-bootstrap';
-import styled from 'styled-components';
 import pageLogo from '../assets/pageLogo.svg';
 
-const Styles = styled.div`
-    .container{
-        margin-top: 100px;
-        color: #A4A4A4;
-    }
-
-    .row{
-        margin-bottom: 50px;
-    }
-    
-    .nav-link{
-        color: #FFFFFF;
-        text-transform: uppercase;
-    }
-`;
-
 export const Footer = () => (
-    <Styles>
-        <Container>
-            <Row className="justify-content-center">
-                <a href="#href">
-                    <img src={pageLogo} alt="logo" height="50px" />
-                </a>
-            </Row>
-            <Row className="justify-content-center">
-                <Nav>
-                    <Nav.Link href="/">Główna</Nav.Link>
-                    <Nav.Link href="/">Oferta</Nav.Link>
-                    <Nav.Link href="/">Kontakt</Nav.Link>
-                </Nav>
-            </Row>
-            <Row className="justify-content-center">
-                <p>Copyright&copy;{new Date().getFullYear()} bronisMateusz</p>
-            </Row>
-        </Container>
-    </Styles>
+    <Container className="footer">
+        <Row className="justify-content-center">
+            <a href="#href">
+                <img src={pageLogo} alt="logo" height="50px" />
+            </a>
+        </Row>
+        <Row className="footer-nav justify-content-center">
+            <Nav className="justify-content-center">
+                <Nav.Item>
+                    <Nav.Link>
+                        <Link to="jumbo">Start</Link>
+                    </Nav.Link>
+                </Nav.Item>
+                <Nav.Item>
+                    <Nav.Link>
+                        <Link to="about-me">O mnie</Link>
+                    </Nav.Link>
+                </Nav.Item>
+                <Nav.Item>
+                    <Nav.Link>
+                        <Link to="offer-section">Oferta</Link>
+                    </Nav.Link>
+                </Nav.Item>
+                <Nav.Item>
+                    <Nav.Link>
+                        <Link to="advantages-section">Korzyści</Link>
+                    </Nav.Link>
+                </Nav.Item>
+                <Nav.Item>
+                    <Nav.Link>
+                         <Link to="contact-section">Kontakt</Link>
+                    </Nav.Link>
+                </Nav.Item>
+            </Nav>
+        </Row>
+        <Row className="justify-content-center">
+            <p>Copyright&copy;{new Date().getFullYear()} bronisMateusz</p>
+        </Row>
+    </Container>
 )
--- src/components/MainNav.js
+++ src/components/MainNav.js
@@ -1,56 +1,42 @@
 import React from 'react';
+import { Link, animateScroll as scroll } from 'react-scroll';
 import { Navbar, Nav, Image} from 'react-bootstrap';
-import styled from 'styled-components';
 import pageLogo from '../assets/pageLogo.svg';
 
-const Styles = styled.div`
-    .navbar{
-        background-color: #141414;
-    }
-
-    .navbar-toggler-icon{
-        
-    }
-
-    .navbar-nav .nav-link {
-        color: #FFFFFF;
-        text-transform: uppercase;
-
-        &:hover{
-            color: #FFFFFF;
-            font-size: 700;
-        }
-    }
-
-    .btn-primary{
-        background-color: transparent;
-        border-color: #FFFFFF;
-        border-radius: 100px;
-        margin-left: 10px;
-        padding: 7px 15px;
-
-        &:hover{
-            border: 2px solid #FFFFFF;;
-            padding: 6px 14px;
-        }
-    }
-`;
-
 export const MainNav = () => (
-    <Styles>
-        <Navbar variant="dark" expand="lg" fixed="top">
-            <Navbar.Brand href="/">
-                <Image src={pageLogo} alt="logo" height="35px" />
-            </Navbar.Brand>
-            <Navbar.Toggle className="border-0" aria-controls="navbar-toggle" />
-            <Navbar.Collapse id="navbar-toggle">
-                <Nav className="ml-auto">
-                    <Nav.Item><Nav.Link href="/">Start</Nav.Link></Nav.Item>
-                    <Nav.Item><Nav.Link href="/">O mnie</Nav.Link></Nav.Item>
-                    <Nav.Item><Nav.Link href="/Offer">Oferta</Nav.Link></Nav.Item>
-                    <Nav.Item><Nav.Link href="/Contact">Kontakt</Nav.Link></Nav.Item>
-                </Nav>
-            </Navbar.Collapse>
-        </Navbar>
-    </Styles>
+    <Navbar variant="dark" expand="lg" fixed="top">
+        <Navbar.Brand href="/">
+            <Image src={pageLogo} alt="logo" height="35px" />
+        </Navbar.Brand>
+        <Navbar.Toggle className="border-0" aria-controls="navbar-toggle" />
+        <Navbar.Collapse id="navbar-toggle">
+            <Nav className="ml-auto">
+                <Nav.Item>
+                    <Nav.Link>
+                        <Link to="jumbo">Start</Link>
+                    </Nav.Link>
+                </Nav.Item>
+                <Nav.Item>
+                    <Nav.Link>
+                        <Link to="about-me">O mnie</Link>
+                    </Nav.Link>
+                </Nav.Item>
+                <Nav.Item>
+                    <Nav.Link>
+                        <Link to="offer-section">Oferta</Link>
+                    </Nav.Link>
+                </Nav.Item>
+                <Nav.Item>
+                    <Nav.Link>
+                        <Link to="advantages-section">Korzyści</Link>
+                    </Nav.Link>
+                </Nav.Item>
+                <Nav.Item>
+                    <Nav.Link>
+                         <Link to="contact-section">Kontakt</Link>
+                         </Nav.Link>
+                </Nav.Item>
+            </Nav>
+        </Navbar.Collapse>
+    </Navbar>
 )
