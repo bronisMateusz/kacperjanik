08e4cf87d635e021793d9c0565d32e93cdcfc923
bronisMateusz <mateusz.bronis99@gmail.com>
2020-06-24 13:56:56 +0200

    Different offer section layout

--- src/App.js
+++ src/App.js
@@ -1,12 +1,8 @@
 import React, { Component } from 'react';
 import './App.css';
-import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
-import { Home } from './Home';
-import { Offer } from './Offer';
-import { Contact } from './Contact';
-import { NoMatch } from './NoMatch';
-import { MainNav } from './components/MainNav';
 
+import { Home } from './Home';
+import { MainNav } from './components/MainNav';
 import { Footer } from './components/Footer';
 
 class App extends Component {
@@ -14,14 +10,7 @@ class App extends Component {
     return (
       <React.Fragment>
         <MainNav />
-          <Router>
-            <Switch>
-              <Route exact path="/" component={Home} />
-              <Route path="/oferta" component={Offer} />
-              <Route path="/kontakt" component={Contact} />
-              <Route component={NoMatch} />
-            </Switch>
-          </Router>
+        <Home />
         <Footer />
       </React.Fragment>
     );
--- src/Home.js
+++ src/Home.js
@@ -1,10 +1,12 @@
 import React from 'react';
-import { Jumbotron as Jumbo, Container, Row, Col, Image, Button} from 'react-bootstrap';
-import jumboImage from './assets/jumboImage.jpg';
+import {Container, Row, Col, Image, Button} from 'react-bootstrap';
+import meImage from './assets/meImage.png';
 import offer1Img from './assets/offer1.jpg';
+import offer2Img from './assets/offer2.jpg';
+import offer3Img from './assets/offer3.jpg';
+import offer4Img from './assets/offer4.jpg';
 import offer5Img from './assets/offer5.jpg';
 import offer6Img from './assets/offer6.jpg';
-import meImage from './assets/meImage.png';
 
 export const Home = () => (
     <Container fluid>
@@ -21,23 +23,23 @@ export const Home = () => (
         <Row className="about-me">
             <Container className="pl-4 pr-4 pt-5">
                 <Row>
-                    <Col lg={6} className="align-self-center pb-5">
+                    <Col md="6" className="align-self-center pb-5">
                         <h2 className="section-title">Cześć,</h2>
                         <p className="pt-4">Mam na imię Kacper i jestem certyfikowanym trenerem personalnym oraz studiuję dietetykę na Warszawskiej uczelni.<br /><br />Jeśli chodzi o moje doświadczenie trenerskie - specjalizuje się w prowadzeniu treningów odpowiednio sprecyzowanych pod daną osobę (budowa siły, typowa hipertrofia mięśniowa, maksymalizacja spalania tkanki tłuszczowej), a także układaniu diet oraz planów treningowych z myślą zarówno o redukcji tkanki tłuszczowej jak i o budowaniu masy mięśniowej. Dodatkowo jestem osobą przygotowującą się do swojego pierwszego startu w zawodach sylwetkowych. W trakcie okresu przygotowań sprawdzam czy teoria ma realne przełożenie na praktykę. Wszystko po to by wiedzieć jak najlepiej współpracować z podopiecznymi.<br /><br />Wszystko będzie indywidualnie dopasowane do Twoich możliwości. Gwarantuję Ci, że wyjście na ćwiczenia to nie będzie przymus, a przyjemność! Zmiana myślenia, pozytywne nastawienie i dobre samopoczucie to cechy, które zagoszczą u Ciebie na co dzień dzięki podjęciu nowej aktywności fizycznej oraz wprowadzeniu zmian żywieniowych.</p>
                     </Col>
-                    <Col lg={6} className="align-self-end">
+                    <Col md="6" className="align-self-end">
                         <Image src={meImage} fluid />
                     </Col>
                 </Row>
             </Container>
         </Row>
         <Row className="offer-section">
-            <Container className="pt-5 pr-4 pb-0 pl-4">
+            <Container className="pt-5 pb-5 p-4">
                 <Row>
-                    <Col lg={{span: 7, order: 1}} className="offer-img align-self-center">
+                    <Col md={{span: 5, order: 1}} className="offer-img align-self-center">
                         <Image src={offer1Img} fluid />
                     </Col>
-                    <Col lg={{span: 5, order: 2}} className="offer-info pt-5 pb-5 align-self-center">
+                    <Col md={{span: 7, order: 2}} className="offer-info pt-5 pb-5 align-self-center">
                         <h6 className="section-subtitle">indywidualnie rozpisana</h6>
                         <h2 className="section-title">dieta</h2>
                         <h3 className="offer-price">129 zł</h3>
@@ -45,10 +47,10 @@ export const Home = () => (
                     </Col>
                 </Row>
                 <Row>
-                    <Col lg={{span: 7, order: 4}} className="offer-img align-self-center">
-                        <Image src={offer1Img} fluid />
+                    <Col md={{span: 5, order: 4}} className="offer-img align-self-center">
+                        <Image src={offer2Img} fluid />
                     </Col>
-                    <Col lg={{span: 5, order: 3}} className="offer-info pt-5 pb-5 align-self-center">
+                    <Col md={{span: 7, order: 3}} className="offer-info pt-5 pb-5 align-self-center">
                         <h6 className="section-subtitle">indywidualnie dopasowany</h6>
                         <h2 className="section-title">trening</h2>
                         <h3 className="offer-price">99 zł</h3>
@@ -56,10 +58,10 @@ export const Home = () => (
                     </Col>
                 </Row>
                 <Row>
-                    <Col lg={{span: 7, order: 5}} className="offer-img align-self-center">
-                        <Image src={jumboImage} fluid />
+                    <Col md={{span: 5, order: 5}} className="offer-img align-self-center">
+                        <Image src={offer3Img} fluid />
                     </Col>
-                    <Col lg={{span: 5, order: 6}} className="offer-info pt-5 pb-5 align-self-center">
+                    <Col md={{span: 7, order: 6}} className="offer-info pt-5 pb-5 align-self-center">
                         <h6 className="section-subtitle">specjalnie dobrany</h6>
                         <h2 className="section-title">pakiet personalny</h2>
                         <h6 className="section-subtitle">(dieta, plan treningowy, suplementacja)</h6>
@@ -68,10 +70,10 @@ export const Home = () => (
                     </Col>
                 </Row>
                 <Row>
-                    <Col lg={{span: 7, order: 8}} className="offer-img align-self-center">
-                        <Image src={jumboImage} fluid />
+                    <Col md={{span: 5, order: 8}} className="offer-img align-self-center">
+                        <Image src={offer4Img} fluid />
                     </Col>
-                    <Col lg={{span: 5, order: 7}} className="offer-info pt-5 pb-5 align-self-center">
+                    <Col md={{span: 7, order: 7}} className="offer-info pt-5 pb-5 align-self-center">
                         <h6 className="section-subtitle">doskonały</h6>
                         <h2 className="section-title">dla wymagających</h2>
                         <h6 className="section-subtitle">(dieta, plan treningowy, suplementacja, opieka trenerska)</h6>
@@ -80,10 +82,10 @@ export const Home = () => (
                     </Col>
                 </Row>
                 <Row>
-                    <Col lg={{span: 7, order: 9}} className="offer-img align-self-center">
+                    <Col md={{span: 5, order: 9}} className="offer-img align-self-center">
                         <Image src={offer5Img} fluid />
                     </Col>
-                    <Col lg={{span: 5, order: 10}} className="offer-info pt-5 pb-5 align-self-center">
+                    <Col md={{span: 7, order: 10}} className="offer-info pt-5 pb-5 align-self-center">
                         <h6 className="section-subtitle">trening</h6>
                         <h2 className="section-title">personalny</h2>
                         <h3 className="offer-price">70 zł</h3>
@@ -91,10 +93,10 @@ export const Home = () => (
                     </Col>
                 </Row>
                 <Row>
-                    <Col lg={{span: 7, order: 12}} className="offer-img align-self-center">
+                    <Col md={{span: 5, order: 12}} className="offer-img align-self-center">
                         <Image src={offer6Img} fluid />
                     </Col>
-                    <Col lg={{span: 5, order: 11}} className="offer-info pt-5 pb-5 align-self-center">
+                    <Col md={{span: 7, order: 11}} className="offer-info pt-5 pb-5 align-self-center">
                         <h6 className="section-subtitle">trening</h6>
                         <h2 className="section-title">personalny</h2>
                         <h6 className="section-subtitle">z dojazdem do domu</h6>
