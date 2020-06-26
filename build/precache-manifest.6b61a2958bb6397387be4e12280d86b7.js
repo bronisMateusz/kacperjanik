1cb60ee7a71821e52965401ee3e6c65b847c8f3f
bronisMateusz <mateusz.bronis99@gmail.com>
2020-06-26 11:35:03 +0200

    Adding firebase and switching to production build

---
+++ build/precache-manifest.6b61a2958bb6397387be4e12280d86b7.js
@@ -0,0 +1,122 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4490c6e9926b79452eccbf64a7fe354f",
+    "url": "/index.html"
+  },
+  {
+    "revision": "07f613eb4192d406606a",
+    "url": "/static/css/2.eb43dee1.chunk.css"
+  },
+  {
+    "revision": "08d0e130ffaacbe7c432",
+    "url": "/static/css/main.c66a9b39.chunk.css"
+  },
+  {
+    "revision": "07f613eb4192d406606a",
+    "url": "/static/js/2.6add7edf.chunk.js"
+  },
+  {
+    "revision": "5ac48c47bb3912b14c2d8de4f56d5ae8",
+    "url": "/static/js/2.6add7edf.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "08d0e130ffaacbe7c432",
+    "url": "/static/js/main.e7a26dfd.chunk.js"
+  },
+  {
+    "revision": "d793caf574016eca9af4",
+    "url": "/static/js/runtime-main.14e1a6b9.js"
+  },
+  {
+    "revision": "5d1ad5fd00ec47f73354d4cd20195495",
+    "url": "/static/media/aboutMeBG.5d1ad5fd.jpg"
+  },
+  {
+    "revision": "5c0464a3413e6d09e1d261defbb9d931",
+    "url": "/static/media/adv1.5c0464a3.svg"
+  },
+  {
+    "revision": "27b0caecec31366b0af0d8bf1c0fb5f7",
+    "url": "/static/media/adv2.27b0caec.svg"
+  },
+  {
+    "revision": "e5c79d91001211dc1aacfd3bfc9878cf",
+    "url": "/static/media/adv3.e5c79d91.svg"
+  },
+  {
+    "revision": "64d46cee375e3bd13553175b5c9ea587",
+    "url": "/static/media/adv4.64d46cee.svg"
+  },
+  {
+    "revision": "ffc873b63ef9906193144f5064919293",
+    "url": "/static/media/adv5.ffc873b6.svg"
+  },
+  {
+    "revision": "4a695032fc64abf5f023f0470cb9a576",
+    "url": "/static/media/adv6.4a695032.svg"
+  },
+  {
+    "revision": "608c8c7dffd53d7a831d700f89fd3866",
+    "url": "/static/media/adv7.608c8c7d.svg"
+  },
+  {
+    "revision": "18bb29ae6141150d3b4590be758d4df5",
+    "url": "/static/media/adv8.18bb29ae.svg"
+  },
+  {
+    "revision": "f2e3af18a931efc75b2fb30c48fa5359",
+    "url": "/static/media/advBG.f2e3af18.jpg"
+  },
+  {
+    "revision": "8aa9436e271dc781281d93e8b8ebd602",
+    "url": "/static/media/fb.8aa9436e.svg"
+  },
+  {
+    "revision": "676ef601b23e77e3f9fbbb2501b6c6c8",
+    "url": "/static/media/ig.676ef601.svg"
+  },
+  {
+    "revision": "b1785379912513f2cfb19135536c2e64",
+    "url": "/static/media/jumboImage.b1785379.jpg"
+  },
+  {
+    "revision": "fc1249ef1bceb82198f41345c9f51a0f",
+    "url": "/static/media/mail.fc1249ef.svg"
+  },
+  {
+    "revision": "e0ce6531a8aba905c8aa7fb83df660bf",
+    "url": "/static/media/meImage.e0ce6531.png"
+  },
+  {
+    "revision": "8b7ce4a9ca8979b7e3fffa28b37ae3fa",
+    "url": "/static/media/offer1.8b7ce4a9.jpg"
+  },
+  {
+    "revision": "feccbb4f756261dd338e6ba33c267997",
+    "url": "/static/media/offer2.feccbb4f.jpg"
+  },
+  {
+    "revision": "be7b48697bf723f20c6a2775f2db612c",
+    "url": "/static/media/offer3.be7b4869.jpg"
+  },
+  {
+    "revision": "aef1e574dff386cf80cd66c6f782f5a3",
+    "url": "/static/media/offer4.aef1e574.jpg"
+  },
+  {
+    "revision": "7b7e4b0e6d0837e57e7a2c67d567aa37",
+    "url": "/static/media/offer5.7b7e4b0e.jpg"
+  },
+  {
+    "revision": "7a384ab6b861b0d460722d13bb73705b",
+    "url": "/static/media/offer6.7a384ab6.jpg"
+  },
+  {
+    "revision": "cc430bc99eabb83d0d5410721f6733f4",
+    "url": "/static/media/pageLogo.cc430bc9.svg"
+  },
+  {
+    "revision": "45ba41e7b6fa9c378a82c9a9e0762858",
+    "url": "/static/media/phone.45ba41e7.svg"
+  }
+]);
